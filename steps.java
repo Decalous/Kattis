ca17a89f0283be712f3e5aa63885ef61c51110fa
Dan Hackman <35549844+Decalous@users.noreply.github.com>
2018-10-10 20:53:30 -0400

    Add files via upload

---
+++ steps.java
@@ -0,0 +1,21 @@
+
+public class steps {
+
+    public static void main(String[] args){
+        int stair = 4;
+        System.out.println(dynamicProgramming(stair));
+    }
+
+    public static int dynamicProgramming(int stair){
+        int[] methods = new int[stair + 1];
+        methods[0] = 1;
+        methods[1] = 1;
+        methods[2] = 2;
+
+        for (int i = 3; i < methods.length; i++){
+            methods[i] = methods[i - 1] + methods[i - 2] + methods[i - 3];
+        }
+
+        return methods[stair];
+    }
+}
