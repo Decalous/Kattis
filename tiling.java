cf0246e891ba44e8fa78474b94f64da72b7d676c
Dan Hackman <35549844+Decalous@users.noreply.github.com>
2018-10-22 21:16:10 -0400

    Add files via upload

---
+++ tiling.java
@@ -0,0 +1,22 @@
+public class tiling {
+
+    public static void main(String[] args){
+        System.out.println(tile(3));
+        System.out.println(tile(4));
+
+    }
+
+    public static int tile(int width){
+        int[] dp = new int[width + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i < width + 1; i++){
+            // the number of ways is split in two categories: one that ends with 1 vertical tile and one that
+            // ends with 2 horizontal tiles
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+
+        return dp[width];
+    }
+}
